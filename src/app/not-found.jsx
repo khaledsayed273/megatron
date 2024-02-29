dfff4e26b9b48b540ca304c357aeacef8022fa77
khaled sayed <khaledsayed273@gmail.com>
2024-02-29 06:59:48 +0200

    first -v

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,6 +5,11 @@ import { useRouter } from 'next/navigation'
 function NotFound() {
   const router = useRouter()
   router.push("/")
+  return (
+    <div>
+      this page not found
+    </div>
+  )
 }
 
 export default NotFound
