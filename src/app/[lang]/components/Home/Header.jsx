b8371c07db8ccd27ad87a31b45f3ae5876cf62e0
khaled sayed <khaledsayed273@gmail.com>
2024-03-07 18:48:07 +0200

    first -v

--- src/app/[lang]/components/Home/Header.jsx
+++ src/app/[lang]/components/Home/Header.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 import headerImg from "../../../../../public/images/header-img.jpeg"
 import Link from 'next/link'
 
-function Header({translate}) {
+function Header({ translate }) {
     return (
         <header data-aos-duration="1000" data-aos="fade-up">
             {/* image */}
-            <div  className='relative w-full h-[388px] rounded-full overflow-hidden'>
+            <div className='relative w-full h-[388px] rounded-full overflow-hidden'>
                 <Image priority src={headerImg} fill alt='HeaderImage' sizes="(max-width: 768px) 100vw " />
             </div>
             {/* center */}
             <div data-aos-duration="1000" data-aos="fade-up">
                 <div className='mt-5 text-center text-white p-3'>
                     <h3 className='text-lg sm:text-2xl lg:text-3xl xl:text-4xl '>
-                    {translate.home.header.title}
+                        {translate.home.header.title}
                     </h3>
                     <p className='mt-4 text-xs md:text-sm text-white/70'>
-                    {translate.home.header.subTitle}
+                        {translate.home.header.subTitle}
                     </p>
                 </div>
                 <div data-aos="fade-up" data-aos-duration="1000" className='flex justify-center mt-3'>
@@ -25,11 +25,12 @@ function Header({translate}) {
                 </div>
             </div>
             {/* End */}
-            <div data-aos-duration="1000" data-aos="fade-up" className='bg-white rounded-3xl flex justify-between items-center flex-col md:flex-row flex-wrap py-2 px-10 my-10 lg:mx-16'>
+            <div data-aos-duration="1000" data-aos="fade-up" className='bg-white rounded-3xl grid md:grid-cols-3 py-2 px-10 md:px-5 my-10 lg:mx-16'>
                 <div data-aos="fade-up" data-aos-duration="1000" className='text-amber-500 text-center my-7'>
                     <h1 className='text-3xl lg:text-5xl font-bold'>100+</h1>
                     <h5 className='text-lg md:text-xl lg:text-2xl '>{translate.home.cart.newUser} </h5>
                 </div>
+
                 <div data-aos="fade-up" data-aos-duration="1000" className='text-amber-500 text-center my-7'>
                     <h1 className='text-3xl lg:text-5xl font-bold'>90+</h1>
                     <h5 className='text-lg md:text-xl lg:text-2xl '>{translate.home.cart.activeUsers}</h5>
@@ -38,6 +39,7 @@ function Header({translate}) {
                     <h1 className='text-3xl lg:text-5xl font-bold '>98%</h1>
                     <h5 className='text-lg md:text-xl lg:text-2xl '>{translate.home.cart.satisfiedCustomers} </h5>
                 </div>
+
             </div>
 
             <div data-aos="fade-up" data-aos-duration="1000" className='flex justify-center'>
