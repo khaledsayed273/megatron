61ef2905ad85481fd75d9ed8c0223c216830dc07
khaled sayed <khaledsayed273@gmail.com>
2024-03-09 03:41:15 +0200

    first -v

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -4,8 +4,6 @@ import Projects from "../shared/projects/Project";
 import GlobalCustomers from "./components/Home/GlobalCustomers";
 import Blogs from "./components/Home/Blogs";
 import Services from "./components/Home/Services";
-
-
 import API from "@/api/API";
 import { getDictionary } from "./dictionaries";
 
@@ -17,7 +15,6 @@ async function getData(lang) {
 }
 
 export const revalidate = +process.env.time; 
-
 
 
 export default async function Home({params}) {
