2a49c891a9913ce4d2eacf6c3552b6a2ecf2ea08
khaled sayed <khaledsayed273@gmail.com>
2024-04-18 04:11:52 +0200

    first commit

--- src/app/[lang]/components/Home/Services.jsx
+++ src/app/[lang]/components/Home/Services.jsx
@@ -32,7 +32,7 @@ function Services() {
                             viewBox="0 0 20 21"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
-                            className="fill-current text-amber-500"
+                            className="fill-current text-orange"
                         >
                             <path
                                 d="M17.5 9.5H4.15625L9.46875 4.09375C9.75 3.8125 9.75 3.375 9.46875 3.09375C9.1875 2.8125 8.75 2.8125 8.46875 3.09375L2 9.65625C1.71875 9.9375 1.71875 10.375 2 10.6562L8.46875 17.2188C8.59375 17.3438 8.78125 17.4375 8.96875 17.4375C9.15625 17.4375 9.3125 17.375 9.46875 17.25C9.75 16.9687 9.75 16.5313 9.46875 16.25L4.1875 10.9062H17.5C17.875 10.9062 18.1875 10.5937 18.1875 10.2187C18.1875 9.8125 17.875 9.5 17.5 9.5Z"
@@ -42,7 +42,7 @@ function Services() {
                     </button>
                 </div>
                 <div className="next-arrow cursor-pointer" onClick={handleNext}>
-                    <button aria-label="next" className="flex h-[40px] w-[40px] items-center justify-center rounded-full border border-stroke bg-amber-500   text-dark transition-all hover:border-transparent hover:drop-shadow-testimonial dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:hover:drop-shadow-none">
+                    <button aria-label="next" className="flex h-[40px] w-[40px] items-center justify-center rounded-full border border-stroke bg-orange   text-dark transition-all hover:border-transparent hover:drop-shadow-testimonial dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:hover:drop-shadow-none">
                         <svg
                             width="20"
                             height="21"
