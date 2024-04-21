974bbdc155538acbdc566e103598e036e2e26350
khaled sayed <khaledsayed273@gmail.com>
2024-04-21 08:58:39 +0200

    first commit

--- src/app/[lang]/projects/components/NavProjects.jsx
+++ src/app/[lang]/projects/components/NavProjects.jsx
@@ -15,11 +15,11 @@ function NavProjects({ services }) {
     }
 
     return (
-        <div data-aos="fade-up" data-aos-duration="1000" className='flex justify-center'>
-            <div className='flex mt-5 flex-wrap justify-start'>
-                <button onClick={() => router.push(`${pathname}`)} className={`me-2 text-xs md:text-sm capitalize ${searchParams.get("category") === null ? "bg-amber-700 text-white" : "bg-white text-yellow-700"} py-1.5  font-bold px-3  rounded-full mt-3`}>all</button>
+        <div data-aos="fade-up" data-aos-duration="1000" className='flex justify-start mt-5 overflow-auto scroll-smooth py-7'>
+            <div className='flex justify-start w-full  items-center '>
+                <button onClick={() => router.push(`${pathname}`)} className={`me-2  text-xs md:text-sm capitalize ${searchParams.get("category") === null ? "bg-amber-700 text-white" : "bg-white text-yellow-700"} py-1.5  font-bold px-3  rounded-full mt-3`}>all</button>
                 {services.map((item) => (
-                    <button onClick={() => handleParams(item.slug)} key={item.id} className={`me-2 text-xs md:text-sm capitalize ${searchParams.get("category") === `${item.slug}` ? "bg-amber-700 text-white" : "bg-white text-yellow-700"}  font-bold px-3 py-1.5 rounded-full mt-3`}>{item.name}</button>
+                    <button onClick={() => handleParams(item.slug)} key={item.id} className={`me-2 whitespace-nowrap text-xs md:text-sm capitalize ${searchParams.get("category") === `${item.slug}` ? "bg-amber-700 text-white" : "bg-white text-yellow-700"}  font-bold px-3 py-1.5 rounded-full mt-3`}>{item.name}</button>
                 ))}
             </div>
         </div>
