a2c4440e5cbbf4984bd548e9530a2ff5b0148279
khaledsayed273 <khaledsayed273@gmail.com>
2024-08-09 08:55:25 +0300

    edit design

--- src/app/[lang]/components/Home/Blogs.jsx
+++ src/app/[lang]/components/Home/Blogs.jsx
@@ -27,7 +27,7 @@ async function Blogs() {
                 <span className='btnOrange text-white flex mx-auto font-bold px-12 py-2 rounded-full capitalize'>blogs</span>
             </div> */}
             <h2 data-aos="fade-up" data-aos-duration="1000" className='capitalize text-center text-white my-10 text-xl lg:text-4xl'>from our latest blogs</h2>
-            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-7 xl:gap-9">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-7 xl:gap-9">
                 {latestBlog?.status && (
                     latestBlog?.data?.map((item) => (
                         <div data-aos="fade-up" data-aos-duration="1000" key={item.id} className='bg-white relative p-5 rounded-xl mx-auto w-11/12 sm:w-2/3 md:w-full'>
